1b9d669e84502177fb30b81b039d762c317a98cd
ennylgnaw <lwang7@stuy.edu>
2018-03-13 22:21:29 -0400

    added avg case csv

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,22 +1,68 @@
+
+	
+
 public class QuickSortTester{
     public static void main(String[] args){
-	int sum = 0;
+
+	//average case
+        int sum1 = 0;
 	for (int i = 0; i < 10000; i += 100){
-	    int[] arr = (QuickSort.buildArray(i, i));
-	    QuickSort.shuffle(arr);
+	    int[] arr1 = (QuickSort.buildArray(i, i));
+	    	
 	    for (int a = 0; a < 20; a++){
+        	QuickSort.shuffle(arr1);
 		long timeInit = System.nanoTime();
 		
-		QuickSort.qsort( arr );
+		QuickSort.qsort( arr1 );
 		
 		long timeFinal = System.nanoTime();
 		long timeOne = (timeFinal - timeInit);
-		sum += timeOne;
+		sum1 += timeOne;
 	    }
-	    System.out.print(i + "," + (sum / 20.) + "\n");
+	    System.out.print(i + "," + (sum1 / 20.) + "\n");
 	}
+	/*
+	//best case: The pivot is the median of the array.
+	int sum2 = 0;
+	for (int i = 0; i < 10000; i += 100){
+	    int[] arr2 = new int[i];
+	    for (int x = 0; x < i; x++) {
+		arr2[x] = x;
+	    }
+	    for (int a = 0; a < 20; a++){
+		long timeInit = System.nanoTime();
+		
+		QuickSort.qsort( arr2 );
+		
+		long timeFinal = System.nanoTime();
+		long timeOne = (timeFinal - timeInit);
+		sum2 += timeOne;
+	    }
+	    System.out.print(i + "," + (sum2 / 20.) + "\n");
+	}
+  
+	//worst case: The pivot is the smallest or largest element of the array. 
+        int sum3 = 0;
+	for (int i = 0; i < 10000; i += 100){
+	    int[] arr3 = new int[i];
+	    for (int x = 0; x < i; x++) {
+		if (x == i/2){
+		    arr3[x] = 0; 
+		}
+		arr3[x] = x + 1;
+	    }
+	    for (int a = 0; a < 20; a++){
+		long timeInit = System.nanoTime();
+		
+		QuickSort.qsort( arr3 );
+      
+		long timeFinal = System.nanoTime();
+		long timeOne = (timeFinal - timeInit);
+		sum3 += timeOne;
+	    }
+	    System.out.print(i + "," + (sum3 / 20.) + "\n");
+	}
+	*/
     }
 }
 	
-	    
-					 
